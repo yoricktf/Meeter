653283eda5f2fbfb780d664b139638d14b144e6e
gabriel pretto <gabrielpretto@yahoo.com.br>
2021-08-25 16:34:50 +0200

    spots mapbox

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :spots, only: [:index, :show]
+
+  get 'spots', to: 'pages#spots'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :spots
+
 end
