e679fb78b7438788fec293fdd4ce60448b43796c
gabriel pretto <gabrielpretto@yahoo.com.br>
2021-09-01 17:02:54 +0200

    working on current location

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -34,9 +34,22 @@ import { initAutocomplete } from '../plugins/init_autocomplete';
 document.addEventListener('turbolinks:load', () => {
   setTimeout(initMapbox, 100);
   initAutocomplete();
+  getLocation()
 })
 
+const x = document.querySelector(".address");
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
 
+function showPosition(position) {
+  x.value = position.coords.latitude + ","
+  + position.coords.longitude;
+}
 // document.addEventListener('turbolinks:load', () => {
 //   // Call your functions here, e.g:
 //   // initSelect2();
