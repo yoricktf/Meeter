0e438d15ac36ffbdced3ccfa4647b2e62a612952
douglas brennan <douglasmbrennan@gmail.com>
2021-09-01 11:10:29 +0200

    forgivemelordihavesinned

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
 
   # before_action :authenticate_user!
 
+  #meta tag setup
+  def default_url_options
+  { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
 end
