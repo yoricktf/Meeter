b2c688ecb60fcc759615ddc568ebdf1cb4e27aec
gabriel pretto <gabrielpretto@yahoo.com.br>
2021-09-02 15:20:41 +0200

    location button

--- app/javascript/plugins/user_location.js
+++ app/javascript/plugins/user_location.js
@@ -1,17 +1,22 @@
-const x = document.querySelector(".address");
+const myAddress = document.querySelector(".address");
 
 
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
   } else {
-    x.value = "Geolocation is not supported by this browser.";
+    myAddress.value = "Geolocation is not supported by this browser.";
   }
 }
 
 function showPosition(position) {
-  x.value = position.coords.latitude + ","
-  + position.coords.longitude;
+  const coordinates = position.coords.latitude + "," + position.coords.longitude;
+
+  fetch(`https://places-dsn.algolia.net/1/places/reverse?aroundLatLng=${coordinates}&hitsPerPage=5&language=en`)
+    .then(response => response.json())
+    .then((data) => {
+      myAddress.value = data.hits[0].locale_names[0];
+    })
 }
 
 
@@ -23,5 +28,8 @@ function buttonClick(){
     })
   }
 }
+
+
+
 
 export { buttonClick };
